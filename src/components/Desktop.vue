f44e6ea90229d8963ecdec5b36950a9fffc62cb5
Maksimk9245 <40x146@gmail.com>
2026-05-29 02:39:19 +0300

    add Document file. Work with browser Simultaneously

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -4,11 +4,16 @@
         v-if="appState.Browser"
         @close="closeApp('Browser')"
     />
+    <Document
+        v-if="appState.Documents"
+        @close="closeApp('Documents')"
+    />
 </div>
 </template>
 
 <script setup lang="ts">
 import Browser from "./TaskBar/Browser.vue";
+import Document from "./TaskBar/Document.vue";
 import { appState, closeApp } from '../store/appState.ts'
 </script>
 
---
+++ src/components/TaskBar/Document.vue
@@ -0,0 +1,21 @@
+<template>
+    <WindowFrame title="Document" :icon="File" @close="$emit('close')">
+        <div class="document--container">
+            <textarea class="notepad-area"></textarea>
+        </div>
+    </WindowFrame>
+</template>
+
+<script setup lang="ts">
+import WindowFrame from "../Windows/WindowFrame.vue";
+import { File } from "lucide-vue-next"
+
+defineEmits(['close'])
+
+
+
+</script>
+
+<style scoped>
+@import '../../style/Document.css';
+</style>
