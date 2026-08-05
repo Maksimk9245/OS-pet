24535b8a9e7483c6ad25a614d0175286a289cf26
Maksimk9245 <40x146@gmail.com>
2026-08-05 18:27:45 +0300

    add css

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -12,12 +12,19 @@
         @minimize="minimizeApp('Documents')"
         @close="closeApp('Documents')"
     />
+    <Images
+        v-if="appState.Images"
+        v-show="!appMinimized.Images"
+        @minimize="minimizeApp('Images')"
+        @close="closeApp('Images')"
+    />
 </div>
 </template>
 
 <script setup lang="ts">
 import Browser from "./TaskBar/Browser.vue";
 import Document from "./TaskBar/Document.vue";
+import Images from "./TaskBar/Images.vue";
 import { appState, closeApp, appMinimized, minimizeApp } from '../store/appState.ts'
 </script>
 
--- src/components/TaskBar/Images.vue
+++ src/components/TaskBar/Images.vue
@@ -7,7 +7,12 @@
                  @minimize="$emit('minimize')"
     >
         <div class="images--container">
-
+            <div class="image--card"
+                 v-for="image in imageList"
+                 :key="image.id"
+            >
+                <img :src="image.url" alt="Image thumbnail"/>
+            </div>
         </div>
     </WindowFrame>
 </template>
@@ -15,9 +20,19 @@
 import WindowFrame from "../Windows/WindowFrame.vue";
 import { File } from "lucide-vue-next"
 import { appZIndex, focusApp } from "../../store/appState.ts";
+import { ref } from "vue";
 
+const imageList = ref([
+    {id: 1, url: 'https://picsum.photos/200?random=1'},
+    {id: 2, url: 'https://picsum.photos/200?random=2'},
+    {id: 3, url: 'https://picsum.photos/200?random=3'},
+    {id: 4, url: 'https://picsum.photos/200?random=4'},
+]);
+
+//TODO selectedimage
 defineEmits ( ["close", "minimize"]);
 </script>
 <style scoped>
 @import '../../style/Images.css';
+
 </style>
